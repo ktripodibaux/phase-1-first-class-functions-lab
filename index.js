a4db63ab3b9fe084c792fac511c3284c8abc27b8
Kurt Baux <ktripodibaux@gmail.com>
2022-06-07 09:58:59 -0400

    commit tree 975d9576cb9bd5680faa70dae1ef4612668604cf

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(arr) {
+    const newArray = [];
+    for (let i =0; i<2; i++) {
+        newArray.push(arr[i]);
+    }
+    return newArray;
+}
+
+function returnLastTwoDrivers(arr) {
+    const newArray = [];
+    for (let i = arr.length -1; i>arr.length-3; i--) {
+        newArray.unshift(arr[i]);
+    }
+    return newArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+// const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()];
+
+function createFareMultiplier(fare) {
+    return function(fare) {
+        return fare * fare;
+    }
+}
+
+function fareDoubler(fare) {
+    return fare*2;
+}
+
+function fareTripler(fare) {
+    return fare*3;
+}
+
+function selectDifferentDrivers (drivers, cb) {
+    return cb(drivers);
+}
